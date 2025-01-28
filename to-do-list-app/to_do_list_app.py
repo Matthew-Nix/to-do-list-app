3973b2c28d7f77fe2588679ce63b5065cf713434
Matthew Nix <fluff.gfg@outlook.com>
2025-01-27 16:15:18 -0800

    Created initial to-list code. User can add text to create new items in list.

--- to-do-list-app/to_do_list_app.py
+++ to-do-list-app/to_do_list_app.py
@@ -1,1 +1,27 @@
+import flet as ft
 
+def main(page: ft.Page):
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        page.update()
+
+    new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+    tasks_view = ft.Column()
+    view = ft.Column(
+        width = 600,
+        controls = [
+            ft.Row(
+                controls = [
+                    new_task,
+                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
+                ],    
+                
+            ),
+        ],  
+    )
+
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
+
+ft.app(main)
